f44e7b874a33dc963e35fa0f85a7c2c679eb26eb
firdaushassansalim <firdaushassan033@gmail.com>
2021-11-08 14:04:41 +0300

    updated documentation

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -42,7 +42,7 @@ class SinglyLinkedList:
     """Represent a singly-linked list."""
 
     def __init__(self):
-        """Initalize a new SinglyLinkedList."""
+        """Initialize a new SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
