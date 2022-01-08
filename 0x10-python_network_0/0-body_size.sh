9553e10fd9242cc5a9b21be1d50d542d7bcae348
Firdaus Salim <firdaushassan033@gmail.com>
2022-01-08 21:10:43 +0300

    task 0

--- 0x10-python_network_0/0-body_size.sh
+++ 0x10-python_network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-# Takes in a URL
-# sends a request to that URL
-# displays the size of the response body
+# ends a request to that URL displays the size of the response body
 curl -s "$1" | wc -c
