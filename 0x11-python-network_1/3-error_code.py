781d2b47b06680f0100414dbe447880f6d5ac9ca
Firdaus Salim <firdaushassan033@gmail.com>
2022-01-09 22:23:28 +0300

    tried to fix bug

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,7 @@
 if __name__ == "__main__":
     import sys
     from urllib import request, error
-    
+
     try:
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('UTF-8'))
