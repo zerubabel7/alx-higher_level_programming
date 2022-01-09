6eb5383bfa0ec66f06a5f099994e395b5b138d52
Firdaus Salim <firdaushassan033@gmail.com>
2022-01-09 22:19:58 +0300

    tried to fix bug

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,12 +4,12 @@
 - sends a request to the URL
 - displays the body of the response (decoded in utf-8).
 """
-import sys
-import urllib.error
-import urllib.request
 
 
 if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+    
     try:
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('UTF-8'))
