b7dd2149b23927bacf573443a47b2e4380166ed3
Firdaus Salim <firdaushassan033@gmail.com>
2022-01-09 20:44:01 +0300

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 
 if __name__ == '__main__':
